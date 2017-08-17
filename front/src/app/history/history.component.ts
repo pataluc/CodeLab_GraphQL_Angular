1fe6b0e4ae5cc772c05a3118b24969616652c21b
antoinecellier <antoine08.cellier@hotmail.fr>
2017-08-17 12:57:26 +0200

    init front

---
+++ front/src/app/history/history.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'history',
+  templateUrl: './history.html',
+  styleUrls: ['./history.css']
+})
+export class HistoryComponent {
+}
---
+++ front/src/app/navbar/navbar.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'navbar',
+  templateUrl: './navbar.html',
+  styleUrls: ['./navbar.css']
+})
+export class NavBarComponent {
+}
