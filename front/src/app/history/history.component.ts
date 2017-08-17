fad509db2565c4cb9d93561c868c0fd1212e0266
antoinecellier <antoine08.cellier@hotmail.fr>
2017-08-17 14:39:08 +0200

    init history page

--- front/src/app/history/history.component.ts
+++ front/src/app/history/history.component.ts
@@ -6,4 +6,24 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./history.css']
 })
 export class HistoryComponent {
+  public messages = [
+    {
+      sender: {
+        lastName: 'Antoine',
+        avatar: 'http://lorempixel.com/50/50/people'
+      },
+      content: 'Hello :)',
+      place: 'Nantes',
+      datetime: '12:00:00 12/08/17'
+    },
+        {
+      sender: {
+        lastName: 'Antoine',
+        avatar: 'http://lorempixel.com/50/50/people'
+      },
+      content: 'Hello :)',
+      place: 'Nantes',
+      datetime: '12:00:00 12/08/17'
+    }
+  ]
 }
