20aaf7b2a7a707d89843fe63300594806ec89ea7
tbetous <tbetous@gmail.com>
2017-10-08 17:54:08 +0200

    Fin du TP1

--- front/src/app/tchat/tchat.service.ts
+++ front/src/app/tchat/tchat.service.ts
@@ -8,8 +8,18 @@ export class TchatService {
     constructor(public apollo: Apollo) {}
 
     getMessages() {
-        return this.apollo.query({
+        return this.apollo.watchQuery({
             query: getRequest
+        })
+    }
+
+    saveMessage(message) {
+        return this.apollo.mutate({
+            mutation: saveRequest,
+            variables: { message },
+            refetchQueries: [{ 
+                query: getRequest
+            }]
         })
     }
 }
@@ -25,6 +35,21 @@ gql`{
         content
         localisation
         date
+        status
+    }
+}`
+
+const saveRequest = 
+gql`mutation saveMessage($message: MessageInput!) {
+    saveMessage(message: $message) {
+        date
+        sender {
+            pseudo
+            firstName
+            lastName
+        }
+        content
+        localisation
         status
     }
 }`
